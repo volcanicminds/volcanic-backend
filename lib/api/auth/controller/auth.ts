1b2952e5609c291d5f966f8c485913ff17cdad1d
dade <dav22@hotmail.it>
2022-11-18 17:26:05 +0100

    change token data

--- lib/api/auth/controller/auth.ts
+++ lib/api/auth/controller/auth.ts
@@ -1,23 +1,27 @@
 import { FastifyReply, FastifyRequest } from 'fastify'
+import { AuthenticatedUser } from '../../../../types/global'
 
 export async function login(req: FastifyRequest, reply: FastifyReply) {
-  const { username, password } = req.data()
+  const { email = '', password = '' } = req.data()
 
-  // log.debug('username ' + username + ' password ' + password)
-
+  // TODO: use UserManagement.find and check password
+  // demo code here
+  const username = email.substr(0, email.indexOf('@')) || 'jerry'
   const roleList = [username === 'admin' ? roles.admin : username === 'vminds' ? roles.backoffice : roles.public]
   const user =
     username !== null
-      ? {
-          sub: 306, // user id
-          name: username // optional, username || email
-        }
+      ? ({
+          id: 306, // user id
+          name: username, // optional
+          email: email,
+          roles: roleList
+        } as AuthenticatedUser)
       : null
 
-  const token = user !== null ? await reply.jwtSign(user) : null
-
-  // log.debug('token ' + token)
-  reply.send({ ok: token !== null, token, roles: roleList.map((r) => r.code) })
+  // TODO: review if email is important to include in token (for a security purpose)
+  // https://www.iana.org/assignments/jwt/jwt.xhtml
+  const token = user !== null ? await reply.jwtSign({ sub: user.id, name: user.name, email: user.email }) : null
+  reply.send({ ...user, token: token || null, roles: roleList.map((r) => r.code) })
 }
 
 export async function demo(req: FastifyRequest, reply: FastifyReply) {
