bbfd6cf7f6f9c1cbcb44bb845b91f8929ba13f9e
dade <dav22@hotmail.it>
2023-05-11 11:42:31 +0200

    enable cors

--- lib/config/plugins.ts
+++ lib/config/plugins.ts
@@ -3,8 +3,41 @@
 module.exports = [
   {
     name: 'cors',
-    enable: false,
-    options: {}
+    enable: true,
+    options: {
+      origin: '*',
+      methods: ['GET', 'POST', 'PUT', 'PATCH', 'DELETE', 'OPTIONS', 'HEAD'],
+      maxAge: 31536000,
+      credentials: true,
+      allowedHeaders: [
+        'Accept',
+        'Accept-Language',
+        'Content-Language',
+        'Content-Type',
+        'Content-Length',
+        'Authorization',
+        'Origin',
+        'v-total',
+        'v-count',
+        'v-page',
+        'v-pageSize',
+        'v-pageCount'
+      ],
+      exposedHeaders: [
+        'Accept',
+        'Accept-Language',
+        'Content-Language',
+        'Content-Type',
+        'Content-Length',
+        'Authorization',
+        'Origin',
+        'v-total',
+        'v-count',
+        'v-page',
+        'v-pageSize',
+        'v-pageCount'
+      ]
+    }
   },
   {
     name: 'rateLimit',
